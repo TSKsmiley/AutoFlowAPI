c33c90a061ab4d75d4d427a5228898348f369b56
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-02-23 08:26:55 +0000

    hey

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,6 +1,6 @@
 import assert from 'assert'
 
-
+//hey
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
