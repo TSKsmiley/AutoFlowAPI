b0db3f3dd7efb1ea87dd1b9ff7d46bb24a98973b
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-02-23 08:06:49 +0000

    replaced jest with mocha

--- src/index.js
+++
@@ -1,7 +0,0 @@
-
-
-// DELETE THIS AFTER SETTING UP ACTIONS
-function sum(a, b) {
-    return a + b;
-  }
-  module.exports = sum;
---
+++ src/test/index.test.js
@@ -0,0 +1,15 @@
+import assert from 'assert'
+
+
+describe('Array', function () {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(0,0);
+    });
+  });
+  describe('#gaming()', function () {
+    it('gaming', function () {
+      assert.equal(0,0);
+    });
+  });
+});
