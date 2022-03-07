077f05644480b714a51e0ec9e94b0c13104db727
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-03-07 08:55:14 +0000

    nu virker det faktisk

--- src/classes/actions/discord.js
+++ src/classes/actions/discord.js
@@ -1,6 +1,5 @@
 import Action from '../action.js';
 import axios from 'axios';
-import chalk from 'chalk';
 
 export class discordWebhook extends Action {
     constructor(caller, hookURL) {
@@ -10,13 +9,13 @@ export class discordWebhook extends Action {
     }
     
     execute(content, username = "AutoFlow", avatar_url = "https://media.discordapp.net/attachments/938428428540076123/948160710918172712/Asset_14x.png?width=765&height=681") {
-        console.log(chalk.yellow("[info] executing webhook action"))
+        console.log("[info] executing webhook action")
         axios.post(this.hookURL, {
             content,
             username,
             avatar_url
         }).then(()=>{
-            console.log(chalk.green("[info] executed webhook action"))
+            console.log("[info] executed webhook action")
         })
     }
 }
