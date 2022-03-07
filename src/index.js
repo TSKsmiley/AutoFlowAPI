0a74f50c8f12f5ba316fb6e6a0addc36bf2b86a7
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-03-07 11:16:52 +0000

    did the port thing
    Co-authored-by: AlbertHald <AlbertHald@users.noreply.github.com>

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@
 import express from 'express';
 import { discordWebhook } from './classes/actions/discord.js'
 //import {actionModel} from './models/actionModel.js'
+import {teamsRouter} from './teams.js'
 
 // Imports > routes
-
+app.use('/actions/teams', teamsRouter);
 
 let testHook = new discordWebhook("/", "https://discord.com/api/webhooks/948867218555412500/dVyR7wK6lb8tdmG0lk2x7NByPRqLNMWOUzUK2mbqUPrltkrniEvlQD25Mr1VuqCDiQH6")
 
@@ -20,6 +21,6 @@ app.get('/:gg', (req, res) => {
 })
 
 
-app.listen(8080, () => {
-  console.log('listening on port http://localhost:8080');
+app.listen(8000, () => {
+  console.log('listening on port http://localhost:8000');
 });
--- src/routes/Actions/teams.js
+++ src/routes/Actions/teams.js
@@ -1,9 +1,8 @@
 import { Router } from "express"
 
 
-
 Router.get('/', (req, res) => {
   res.send("Hello World!");
 })
 
-export const actionRouter= Router;
+export const teamsRouter= Router;
