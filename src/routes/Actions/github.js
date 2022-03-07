8edb8646db0ed5acbb782e27709932fcee10b825
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-03-07 13:08:56 +0000

    action

--- src/routes/Actions/github.js
+++ src/routes/Actions/github.js
@@ -13,7 +13,7 @@ Router.get('/', (req, res) => {
 })
 
 Router.post('/', (req, res) => {
-    testHook.execute("```json\n" + req.body + "```");
+    testHook.execute("```json\n" + req.body.action + "```");
     console.log(request.body);
     res.status(200).send('ok');
 })
