4a653f85fce9e653ecbc646f75f857e466ec479a
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-03-07 13:05:54 +0000

    yeah

--- src/routes/Actions/github.js
+++ src/routes/Actions/github.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { request } from "express";
-import { discordWebhook } from "../../classes/actions/discord";
+import { discordWebhook } from "../../../classes/actions/discord.js";
 
 const testHook = new discordWebhook("/", "https://discord.com/api/webhooks/948867218555412500/dVyR7wK6lb8tdmG0lk2x7NByPRqLNMWOUzUK2mbqUPrltkrniEvlQD25Mr1VuqCDiQH6");
 
