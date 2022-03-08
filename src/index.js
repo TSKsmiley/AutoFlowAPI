359d1a66aabeda6f242eb80870fcab357a001972
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-03-08 08:11:58 +0000

    yes

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import express from 'express';
 const app = express();
 import {githubAction} from './routes/Actions/github.js'
 import 'dotenv/config';
+import {bodyParser} from 'body-parser';
 
 import { discordWebhook } from './classes/actions/discord.js'
 //import {actionModel} from './models/actionModel.js'
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Imports > routes
 app.use('/actions/github', githubAction);
