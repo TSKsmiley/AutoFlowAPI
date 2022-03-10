2f5f35a6aad39f7d1b20000a6f0461aead25cb99
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-03-10 08:43:30 +0000

    gmaign

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 /// Imports
 import express from 'express';
 import 'dotenv/config';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
-import {userModel} from './models/actionModel.js';
+import cors  from "cors";
 
 import { webpanelHandler } from './routes/webpanelHandler.js';
-import { GithubAction } from './routes/webhooks/github.js'
+import { GithubAction } from './routes/webhooks/github.js';
 
-import { DiscordWebhook } from './classes/webhooks/discord.js'
-//import {actionModel} from './models/actionModel.js'
+import {userModel} from './models/actionModel.js';
 
 const app = express();
 
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// cors so that we can acces the api form the game that is on a different subdomain
+app.use(cors());
+app.use(express.json());
 
 // Imports > routes
 app.use('/actions/github', GithubAction);
