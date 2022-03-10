d91d6f2b55a250da9918ef9cdcb02d7f91fd0b05
AlbertHald <albert24122@gmail.com>
2022-03-10 16:27:52 +0100

    Busk har været med til mail

--- src/classes/webhooks/sendMail.js
+++ src/classes/webhooks/sendMail.js
@@ -1,5 +1,6 @@
 import nodemailer  from "nodemailer";
 import Action from '../action.js';
+import { DiscordWebhook } from "./discord.js";
 
 //Brugeren angiver mail
 //Angiver brugeren hvilken action der skal aktivere et mail send.
@@ -8,29 +9,51 @@ import Action from '../action.js';
 //Brugeren skal inputte deres mail,
 
 export class Nodemail extends Action {
-    constructor(caller, mailTo, mailSubject, mailText)
+    constructor(mailTo, mailSubjectDefault = "Nothing to see", mailTextDefault = "Just checking in! :)") {
+        super();
+        this.mailTo;
+        this.mailSubjectDefault = mailSubjectDefault;
+        this.mailTextDefault = mailTextDefault;
+    }
+
+    execute(action, mailTo = this.mailTo, mailSubject = this.mailSubjectDefault, mailText = this.mailTextDefault){
+        
+        switch (action) {
+            case ("sendMail"):
+
+                //The sender authentication
+                var transporter = nodemailer.createTransport({
+                  service: 'gmail',
+                  auth: {
+                    user: process.env.MAIL_ADRESS,
+                    pass: process.env.MAIL_PASSWORD
+                  }
+                })
+                
+                //Who to send to and what to send
+                var mailOptions = {
+                    from: process.env.MAIL_ADRESS,
+                    to: mailTo,
+                    subject: mailSubject,
+                    text: mailText
+                }
+
+                transporter.sendMail(mailOptions, function(error, info) {
+                  if (error) {
+                    console.log(error);
+                  } else {
+                    console.log('Email sent: ' + info.response);
+                  }
+                });
+                break;
+            //If more, add
+            default:
+                console.log("Help TEAM, its wrong :(");
+
+                const discordMailFail = new DiscordWebhook(process.env.DISCORD_WEBHOOK_ERROR)
+                
+                discordMailFail.execute("embedMessage", [{Title: "Error", description: `De skrev: "${action}" i sendMail.js`}]);
+                break;
+        }
+    }
 }
-
-
-var transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-          user: 'AutoFlow@gmail.com',
-          pass: 'AdminPassword'
-        }
-      });
-      
-      var mailOptions = {
-        from: 'AutoFlow@gmail.com',
-        to: 'myfriend@yahoo.com',
-        subject: 'Sending Email using Node.js',
-        text: 'That was easy!'
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
