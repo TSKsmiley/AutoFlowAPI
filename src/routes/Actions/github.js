22344bc15fc5f6d08e5a12e3c49f96f50b64ef71
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-03-10 08:43:23 +0000

    gaming
    Co-authored-by: BaBrixx <BaBrixx@users.noreply.github.com>

---
+++ src/routes/Actions/github.js
@@ -0,0 +1,37 @@
+import express from "express";
+import { DiscordWebhook } from "../../classes/actions/discord.js";
+import 'dotenv/config';
+
+
+const testHook = new DiscordWebhook("/", process.env.DISCORD_WEBHOOK_TEST);
+
+const Router = express.Router();
+
+
+Router.get('/', (req, res) => {
+    console.log(req.body);
+  res.status(200).send('ok'); 
+})
+
+Router.post('/:userID', (req, res) => {
+    //todo: dat action stuffs
+    testHook.execute("embedMessage",[{
+        title: "Test",
+        fields: [
+            {
+                "name": "Action:",
+                "value": req.body.action,
+                "inline": true
+              },
+              {
+                "name": "repository:",
+                "value": req.body.repository.name,
+                "inline": true
+              }
+        ]
+    }] );
+    console.log(req.body);
+    res.status(200).send('ok');
+})
+
+export const GithubAction = Router;
