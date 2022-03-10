2079cb479f4824578c9bbd63b66a6e1714c81a51
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-03-10 09:53:26 +0000

    gaming

--- src/routes/webpanelHandler.js
+++ src/routes/webpanelHandler.js
@@ -23,6 +23,6 @@ async function verify(token) {
       audience: process.env.GOOGLE_TOKEN,  // Specify the CLIENT_ID of the app that accesses the backend
   });
   const payload = ticket.getPayload();
-  const userid = payload.email;
+  const userid = payload.aud;
   return userid;
 }
