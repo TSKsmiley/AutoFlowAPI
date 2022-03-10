303f2e75e5082830799bf7a535222910138a2766
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-03-10 13:20:39 +0000

    yes

--- src/models/actionModel.js
+++ src/models/actionModel.js
@@ -10,7 +10,7 @@ const action = new Schema({
 const flow = new Schema({
     platform: String,
     platformAction: String,
-    action: [action]
+    actions: [action]
 });
 
 const userSchema = new Schema({
