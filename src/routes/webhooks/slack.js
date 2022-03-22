1745d9d8aab74003260364263fb32551e9d7d263
AlbertHald <albert24122@gmail.com>
2022-03-22 09:03:12 +0100

    Made ok

--- src/routes/webhooks/slack.js
+++ src/routes/webhooks/slack.js
@@ -9,7 +9,7 @@ const Router = express.Router();
 
 Router.get('/', (req, res) => {
     console.log(req.body);
-    res.status(200).send(req.body.challenge); 
+    res.status(200).send(req.body.challenge).send('ok'); 
 })
 
 export const slackAPI = Router;
