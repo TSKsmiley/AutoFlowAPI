c10e8f5d32a6b3c527cd9503aa4993cdb4662fc6
AlbertHald <albert24122@gmail.com>
2022-03-28 14:59:12 +0200

    fjern http

--- src/routes/webhooks/slack.js
+++ src/routes/webhooks/slack.js
@@ -1,7 +1,7 @@
 // Initialize using signing secret from environment variables
 import { createEventAdapter } from '@slack/events-api';
 const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
-const port = 3001;
+
 
 // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
 slackEvents.on('message', (event) => {
@@ -10,11 +10,5 @@ slackEvents.on('message', (event) => {
 
 // Handle errors (see `errorCodes` export)
 slackEvents.on('error', console.error);
-
-// Start a basic HTTP server
-slackEvents.start(port).then(() => {
-    // Listening on path '/slack/events' by default
-    console.log(`server listening on port ${port}`);
-  });
 
 export const slackAPIsej = slackEvents;
