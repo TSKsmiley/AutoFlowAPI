9ec6440bc429a5c0c041fd1051d47e622534ee9a
AlbertHald <albert24122@gmail.com>
2022-03-28 16:15:02 +0200

    asdf

--- src/routes/webhooks/slack.js
+++ src/routes/webhooks/slack.js
@@ -1,6 +1,6 @@
 // Initialize using signing secret from environment variables
 import { createEventAdapter } from '@slack/events-api';
-import { DiscordWebhook } from '../../classes/webhooks/discord';
+import { DiscordWebhook } from "../../classes/webhooks/discord.js";
 const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 
 
