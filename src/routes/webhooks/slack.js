9538165c589443857af9fa4ee8dcca4f69d8af56
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-04-07 10:59:29 +0000

    attempted to make slack do stuff

--- src/routes/webhooks/slack.js
+++ src/routes/webhooks/slack.js
@@ -7,12 +7,19 @@ const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 
 // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
 slackEvents.on('message', (event) => {
-    console.log(`Received a message event: user: ${event} in channel: ${event.channel} says: ${event.text}`);
     console.log(`-------------------------------------------------------`);
     console.log(event);
     console.log(`-------------------------------------------------------`);
 
-    //UserDB.findAllSlackUsersById()
+    UserDB.findAllSlackUsersById(event.team).then(users => {
+        for(let user of users){
+            for(let flow of user.flows){
+                if(flow.arguments.includes(event.team)){
+                    FlowHandler.executeFlowDirect(user,flow,event.type,event);
+                }
+            }
+        }
+    });
 
     //let discWebhook = new DiscordAction(process.env.DISCORD_WEBHOOK_TEST);
     //discWebhook.execute("sendMessage", [`Received a message from SLACK: user: ${event.user} in channel: ${event.channel} says: ${event.text}`]);
