aeb2cafe5ce92e14c601059d580d2dc8909f0d69
Arnar Freyr <Arnarfreyr29@gmail.com>
2022-04-07 10:46:28 +0000

    gaming

--- src/routes/webhooks/slack.js
+++ src/routes/webhooks/slack.js
@@ -8,11 +8,14 @@ const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET);
 // Attach listeners to events by Slack Event "type". See: https://api.slack.com/events/message.im
 slackEvents.on('message', (event) => {
     console.log(`Received a message event: user: ${event} in channel: ${event.channel} says: ${event.text}`);
+    console.log(`-------------------------------------------------------`);
+    console.log(event);
+    console.log(`-------------------------------------------------------`);
 
     //UserDB.findAllSlackUsersById()
 
-    let discWebhook = new DiscordAction(process.env.DISCORD_WEBHOOK_TEST);
-    discWebhook.execute("sendMessage", [`Received a message from SLACK: user: ${event.user} in channel: ${event.channel} says: ${event.text}`]);
+    //let discWebhook = new DiscordAction(process.env.DISCORD_WEBHOOK_TEST);
+    //discWebhook.execute("sendMessage", [`Received a message from SLACK: user: ${event.user} in channel: ${event.channel} says: ${event.text}`]);
 });
 
 // Handle errors (see `errorCodes` export)
